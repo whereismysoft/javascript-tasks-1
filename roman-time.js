5e109d5c4e1b0f2d773af57b957bbc30b07b8d90
whereismysoft <lomolobus@rambler.ru>
2017-02-13 18:50:36 +0300

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,8 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
+If (minutes >= 60) {
+  alert ('введите корректное время (минуты)');}
+Else If (hours > 24) {
+  alert ('введите корректное время (часы)');}
 
 // Немного замечательного кода и магии
